7882580a2b3e46b2f33f62498c7f585adfdf3707
Shabana Mirza <shabanamirza2010@hotmail.com>
2019-02-20 23:00:21 +0000

    polymorphism section complete

--- homeworkAnswers.js
+++ homeworkAnswers.js
@@ -1,24 +1,34 @@
+
 # Polymorphism & Composition Homework - Quiz
 
 # Polymorphism
 
 1. What does the ___word___ 'polymorphism' mean?
+ITS THE ABILITY OF AN OBJECT TO TAKE ON MANY FORMS
 
 2. What does it mean when we apply polymorphism to OO design? Give a simple Java example.
+IN JAVA - AN OBJECT THAT CAN PASS MORE THAN ONE IS-A TEST IS CONSIDERED POLYMORPHIC.
 
 3. What can we use to implement polymorphism in Java?
+WE CAN USE INHERITANCE, ABSTRACT METHODS AND OVERRIDING TO IMPLEMENT POLYMORPHISM IN JAVA.
 
 4. How many 'forms' can an object take when using polymorphism?
+TWO FORMS 1. METHOD OVERLOADING: VARIOUS METHODS WITH THE SAME NAME ARE PRESENT IN A CLASS.
+2. METHOD OVERRIDING:CHILD CLASS OVERRIDES  A METHOD OF ITS PARENT.
 
 5. Give an example of when you could use polymorphism.
-
+IN A SUPERCLASS OF ANIMALS, YOU CAN HAVE "ANIMAL MAKES A SOUND".
+FOR THE CAT SUBCLASS YOU CAN OVERRIDE WITH "CAT MAKES MEOW SOUND"
+FOR THE OTHER SUBCLASS DOG - YOU CAN OVERRIDE WITH "DOG MAKE WOOF SOUND".
 
 
 # Composition
 
 6. What do we mean by 'composition' in reference to object-oriented programming?
+COMPOSITION IS THE DESIGN TECHNIQUE TO IMPLEMENT "HAS-A" RELATIONSHIP IN CLASSES.
 
 7. When would you use composition? Provide a simple example in Java.
+
 
 8. What is/are the advantage(s) of using composition?
 
